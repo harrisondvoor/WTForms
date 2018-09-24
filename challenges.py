ec7b424146288ed3af11d1dd81a5e9a0aa538e49
Harrison Dvoor <dvoor@umich.edu>
2018-09-24 09:48:40 -0400

    Uploading challenges.py file that we worked on in discussion today

--- challenges.py
+++ challenges.py
@@ -1,4 +1,10 @@
-#import statements go here 
+from flask import Flask, request, render_template, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import Required
+
+import requests
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -8,19 +14,30 @@ app.debug = True
 def home():
     return "Hello, world!"
     
-#create class to represent WTForm that inherits flask form
+class itunesForm(FlaskForm):
+    artist = StringField('Enter Artist', validators=[Required()])
+    api = IntegerField('Enter the number of results?', validators=[Required()])
+    email = IntegerField('Enter your email', validators=[Required(), Email()])
+    submit = SubmitField('Submit')
 
 @app.route('/itunes-form')
 def itunes_form():
-    #what code goes here?
+    simpleForm = itunesForm()
     return render_template('itunes-form.html', form=simpleForm) # HINT : create itunes-form.html to represent the form defined in your class
 
 @app.route('/itunes-result', methods = ['GET', 'POST'])
 def itunes_result():
-    #what code goes here?
-    # HINT : create itunes-results.html to represent the results and return it
+    form = itunesForm(request.form)
+    if method == "POST" and form.validate_on_submit():
+        artist = form.artist.data
+        api = form.api.data
+        params = {}
+        params['term'] = artist
+        params['limit'] = api
+        response = requests.get('https://itunes.apple.com/search', params = params)
+        response_py = json.loads(response.text)
     flash('All fields are required!')
-    return redirect(url_for('itunes_form')) #this redirects you to itunes_form if there are errors
+    return render_template('itunes-result.html', result_html = response_py)
 
 if __name__ == '__main__':
     app.run()
